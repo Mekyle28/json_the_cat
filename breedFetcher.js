87342370da26253c78a9f9bc083500000ad1f9b1
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-09 23:16:38 -0400

    tur nin to function and movved stuff to index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,27 +1,37 @@
 const needle = require('needle');
 
-const catBreed = process.argv[2];
+
+const fetchBreedDescription = function(catBreed, callback) {
+  needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`, callback);
+};
+
+module.exports = { fetchBreedDescription };
 
 
 
-needle.get(`https://api.thecatapi.cm/v1/breeds/search?q=${catBreed}`, (error, response, body) => {
 
-  if (error !== null) {
-    console.log('error: ', error);
-    return;
-  } // Print the error if one occurred
-  if (response.statusCode === 404) {
-    console.log('status code: ', response.statusCode);
-    return;
-  }
-  if (body.length === 0) {
-    console.log("sorry, cat breed is not found");
-    return;
-  }
-  if (response.statusCode === 200) {
-    //console.log('body:', body); // Print the HTML for the Google homepage.
-    let breedDescription = (body[0]["description"]);
-    console.log(breedDescription);
-  }
-});
+
+
+
+
+// needle.get(`https://api.thecatapi.cm/v1/breeds/search?q=${catBreed}`, (error, response, body) => {
+
+//   if (error !== null) {
+//     console.log('error: ', error);
+//     return;
+//   } // Print the error if one occurred
+//   if (response.statusCode === 404) {
+//     console.log('status code: ', response.statusCode);
+//     return;
+//   }
+//   if (body.length === 0) {
+//     console.log("sorry, cat breed is not found");
+//     return;
+//   }
+//   if (response.statusCode === 200) {
+//     //console.log('body:', body); // Print the HTML for the Google homepage.
+//     let breedDescription = (body[0]["description"]);
+//     console.log(breedDescription);
+//   }
+// });
 
