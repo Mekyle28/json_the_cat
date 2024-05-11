429a71ee17efe4d080273b4e9cf3d545977ad8a7
Mackenzie Hamelin <mackenziehamelin1@hotmail.com>
2024-05-11 19:10:31 -0400

    redo the callback set up to make sense

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
 const { fetchBreedDescription } = require('./breedFetcher');
 
 
-const catBreed = process.argv[2];
+const breedName = process.argv[2];
 
-fetchBreedDescription(catBreed, (error, response, body) => {
 
-  if (error !== null) {
-    console.log('error: ', error);
-    return error;
-  } // Print the error if one occurred
-  if (response.statusCode === 404) {
-    console.log('status code: ', response.statusCode);
-    return;
-  }
-  if (body.length === 0) {
-    console.log("sorry, cat breed is not found");
-    return;
-  }
-  if (response.statusCode === 200) {
-    let breedDescription = (body[0]["description"]);
-    console.log(breedDescription);
-    return breedDescription;
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
   }
 });
+
+// if (error !== null) {
+//   console.log('error: ', error);
+//   return error;
+// } // Print the error if one occurred
+  
+// const desc = (body.body[0]["description"]);
+// console.log(desc);
+// return desc;
+
+
+
+
+
+// fetchBreedDescription(catBreed, (error, response, body) => {
+
+//   if (error !== null) {
+//     console.log('error: ', error);
+//     return error;
+//   } // Print the error if one occurred
+//   if (response.statusCode === 404) {
+//     console.log('status code: ', response.statusCode);
+//     return;
+//   }
+//   if (body.length === 0) {
+//     console.log("sorry, cat breed is not found");
+//     return;
+//   }
+//   if (response.statusCode === 200) {
+//     let breedDescription = (body[0]["description"]);
+//     console.log(typeof breedDescription)
+//     console.log(breedDescription);
+//     return breedDescription;
+//   }
+// });
